cb01dcea9cd2e88a48a93b6431f9b5105991fb69
er-omsonar <er.omsonar@gmail.com>
2025-12-06 19:51:26 +0530

    starting the movie search project

---
+++ react-movie-search/src/App.jsx
@@ -0,0 +1,22 @@
+import  './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <Text/>
+      <Text/>
+    </>
+  )
+}
+
+function Text (){
+  return(
+    <div>
+      <p>hello bhai</p>
+    </div>
+  )
+}
+
+export default App
