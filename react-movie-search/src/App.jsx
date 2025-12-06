2aa0caaf0eb2b1f895a394256317173b16aa1744
er-omsonar <er.omsonar@gmail.com>
2025-12-06 20:16:27 +0530

    designing the movie card component

--- react-movie-search/src/App.jsx
+++ react-movie-search/src/App.jsx
@@ -1,21 +1,12 @@
 import  './App.css'
-
+import MovieCard from './components/MovieCard'
 function App() {
 
 
   return (
     <>
-      <Text/>
-      <Text/>
+     <MovieCard movie={{title: 'oms flim', release_date: '2025'}}/>
     </>
-  )
-}
-
-function Text (){
-  return(
-    <div>
-      <p>hello bhai</p>
-    </div>
   )
 }
 
