62bae700af4a36e0efeeef147f45ba375811ba34
er-omsonar <er.omsonar@gmail.com>
2025-12-07 17:25:06 +0530

    learned about routing

--- react-movie-search/src/App.jsx
+++ react-movie-search/src/App.jsx
@@ -2,17 +2,19 @@ import  './App.css'
 import Home from './pages/Home'
 import Favorites from './pages/favorites'
 import { Route,Routes } from 'react-router-dom'
+import NavBar from './components/NavBar'
 function App() {
 
   return (
-    <>
+    <div>
+        <NavBar/>
      <main className='main-content'>
       <Routes>
         <Route path='/' element= {<Home/>}/>
         <Route path='/favorites' element={<Favorites/>}/>
       </Routes>
      </main>
-    </>
+    </div>
   )
 }
 
